334034e05a5136b5155bb79c47a364f445f697c9
Shark <sharktheone@proton.me>
2024-10-29 14:32:24 +0100

    print gameboard and alt gameboard to the terminal

--- lib/dgol.ex
+++ lib/dgol.ex
@@ -14,13 +14,28 @@ defmodule Dgol do
 
   """
   def hello do
+    IO.puts("Initial game:")
+
+    Terminal.clear_screen()
 
     Game.print_game(Game.get_initial_game())
 
-    IO.puts("Hello, world!")
-    :world
+
+    Enum.each(1..100, fn _ ->
+      :timer.sleep(100)
+      Terminal.clear_screen()
+      Game.print_game(Game.get_initial_game_alt())
+      :timer.sleep(100)
+      Terminal.clear_screen()
+      Game.print_game(Game.get_initial_game())
+    end)
   end
 end
 
+defmodule Terminal do
+  def clear_screen do
+    IO.write(IO.ANSI.clear())
+  end
+end
 
 Dgol.hello()
